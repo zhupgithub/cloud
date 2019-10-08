355037c0546902ad9aa617dc802c5f032862ece8
zhupgithub <1539543015@qq.com>
2019-10-08 16:53:50 +0800

    数据库和bean属性值不对应

--- mybatisplus/src/main/java/com/zhupeng/entity/Permission.java
+++ mybatisplus/src/main/java/com/zhupeng/entity/Permission.java
@@ -2,6 +2,9 @@ package com.zhupeng.entity;
 
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.baomidou.mybatisplus.extension.activerecord.Model;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
 import java.io.Serializable;
 
 /**
@@ -17,11 +20,13 @@ public class Permission extends Model<Permission> {
 
     private static final long serialVersionUID = 1L;
 
+	@TableId(type = IdType.AUTO)
     private Integer pid;
 
     /**
      * 权限名称
      */
+	@TableField(value = "name")
     private String pname;
 
     public Integer getPid() {
--- mybatisplus/src/main/java/com/zhupeng/entity/User.java
+++ mybatisplus/src/main/java/com/zhupeng/entity/User.java
@@ -1,7 +1,10 @@
 package com.zhupeng.entity;
 
 import java.io.Serializable;
-
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
 /**
  * user
  * @author 朱朋
